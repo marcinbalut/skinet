13219439ef7601fba44a083f390a98f7212fd46e
Marcin Balut <marcinbalutpoland@gmail.com>
2022-03-25 18:04:21 +0100

    Added breadcrumbs

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,16 +4,14 @@ import { NotFoundComponent } from './core/not-found/not-found.component';
 import { ServerErrorComponent } from './core/server-error/server-error.component';
 import { TestErrorComponent } from './core/test-error/test-error.component';
 import { HomeComponent } from './home/home.component';
-import { ProductDetailComponent } from './shop/product-detail/product-detail.component';
-import { ShopComponent } from './shop/shop.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'test-error', component: TestErrorComponent},
-  {path: 'server-error', component: ServerErrorComponent},
-  {path: 'not-found', component: NotFoundComponent},
-  {path: 'shop', loadChildren: () => import('./shop/shop.module').then(module => module.ShopModule)},
-  {path: "**", redirectTo: '', pathMatch: 'full'}
+  {path: '', component: HomeComponent, data: {breadcrumb: 'Home'}},
+  {path: 'test-error', component: TestErrorComponent, data: {breadcrumb: 'Test Errors'}},
+  {path: 'server-error', component: ServerErrorComponent, data: {breadcrumb: 'Server Error'}},
+  {path: 'not-found', component: NotFoundComponent, data: {breadcrumb: 'Not found'}},
+  {path: 'shop', loadChildren: () => import('./shop/shop.module').then(module => module.ShopModule), data: {breadcrumb: 'Shop'}},
+  {path: "**", redirectTo: 'not-found', pathMatch: 'full'}
 ];
 
 @NgModule({
--- client/src/app/shop/shop-routing.module.ts
+++ client/src/app/shop/shop-routing.module.ts
@@ -6,7 +6,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {path: '', component: ShopComponent},
-  {path: ':id', component: ProductDetailComponent}
+  {path: ':id', component: ProductDetailComponent, data: {breadcrumb: {alias: 'productDetails'}}}
 ];
 
 
