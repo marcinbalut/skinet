a2086041dc62e58d49993a7c1e53b80c735ec883
Marcin Balut <marcinbalutpoland@gmail.com>
2022-03-25 13:34:21 +0100

    Added TestErrorComponent

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TestErrorComponent } from './core/test-error/test-error.component';
 import { HomeComponent } from './home/home.component';
 import { ProductDetailComponent } from './shop/product-detail/product-detail.component';
 import { ShopComponent } from './shop/shop.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
+  {path: 'test-error', component: TestErrorComponent},
   {path: 'shop', loadChildren: () => import('./shop/shop.module').then(module => module.ShopModule)},
   {path: "**", redirectTo: '', pathMatch: 'full'}
 ];
