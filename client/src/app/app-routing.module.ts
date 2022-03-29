81ff9b81f260884a37e22cc874ab6a9635b59857
Marcin Balut <marcinbalutpoland@gmail.com>
2022-03-29 15:49:05 +0200

    Added checkout module, service and component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -12,6 +12,7 @@ const routes: Routes = [
   {path: 'not-found', component: NotFoundComponent, data: {breadcrumb: 'Not found'}},
   {path: 'shop', loadChildren: () => import('./shop/shop.module').then(module => module.ShopModule), data: {breadcrumb: 'Shop'}},
   {path: 'basket', loadChildren: () => import('./basket/basket.module').then(module => module.BasketModule), data: {breadcrumb: 'Basket'}},
+  {path: 'checkout', loadChildren: () => import('./checkout/checkout.module').then(module => module.CheckoutModule), data: {breadcrumb: 'Checkout'}},
   {path: "**", redirectTo: 'not-found', pathMatch: 'full'}
 ];
 
