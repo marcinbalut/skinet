691398281ac88e6441fa3209813b619ac65bfade
Marcin Balut <marcinbalutpoland@gmail.com>
2022-04-05 15:23:38 +0200

    Added register form

--- client/src/app/core/interceptors/loading.interceptor.ts
+++ client/src/app/core/interceptors/loading.interceptor.ts
@@ -12,9 +12,12 @@ export class LoadingInterceptor implements HttpInterceptor {
     constructor(private busyService: BusyService) {}
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>{
+        if (!req.url.includes('emailexists')) {
+            this.busyService.busy();
+        }
         this.busyService.busy();
         return next.handle(req).pipe(
-            delay(1000),
+            delay(500),
             finalize(() => {
                 this.busyService.idle();
             })
