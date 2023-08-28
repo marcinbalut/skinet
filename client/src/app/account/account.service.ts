872cf03241555f093153f5d3f28831386499f5a8
marcinbalut <marcinbalutpoland@gmail.com>
2023-08-28 15:35:38 +0200

    Hooked up client side loging functionality

--- client/src/app/account/account.service.ts
+++ client/src/app/account/account.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, map } from 'rxjs';
+import { BehaviorSubject, map, of } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { User } from '../shared/models/user';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
 
 @Injectable({
@@ -14,6 +14,28 @@ export class AccountService {
   currentUser$ = this.currentUserSource.asObservable();
   
   constructor(private http: HttpClient, private router: Router) { }
+
+  loadCurrentUser(token: string | null) {
+    if (token == null) {
+      this.currentUserSource.next(null);
+      return of(null);
+    }
+
+    let headers = new HttpHeaders();
+    headers = headers.set('Authorization', `Bearer ${token}`);
+
+    return this.http.get<User>(this.baseUrl + 'account', {headers}).pipe(
+      map(user => {
+        if (user) {
+          localStorage.setItem('token', user.token);
+          this.currentUserSource.next(user);
+          return user;
+        } else {
+          return null;
+        }
+      })
+    )
+  }
 
   login(values: any) {
     return this.http.post<User>(this.baseUrl + 'account/login', values).pipe(
