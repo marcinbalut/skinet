dc404f2ae2d7243a7dd0281f858007b67d7cbf7c
marcinbalut <marcinbalutpoland@gmail.com>
2023-09-05 17:03:46 +0200

    Updated loading interceptor

--- client/src/app/core/interceptors/loading.interceptor.ts
+++ client/src/app/core/interceptors/loading.interceptor.ts
@@ -19,7 +19,7 @@ export class LoadingInterceptor implements HttpInterceptor {
     }
     
     return next.handle(request).pipe(
-      delay(1000),
+      delay(200),
       finalize(() => this.busyService.idle())
     );
   }
