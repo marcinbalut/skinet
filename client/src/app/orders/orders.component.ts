6361bb9d4e04547c6002e5094af7eecd765cb1b7
marcinbalut <marcinbalutpoland@gmail.com>
2023-09-05 15:01:31 +0200

    Added essential implementation of orders component

--- client/src/app/orders/orders.component.ts
+++ client/src/app/orders/orders.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Order } from '../shared/models/order';
+import { OrdersService } from './orders.service';
 
 @Component({
   selector: 'app-orders',
   templateUrl: './orders.component.html',
   styleUrls: ['./orders.component.scss']
 })
-export class OrdersComponent {
+export class OrdersComponent implements OnInit {
+  orders: Order[] = [];
+
+  constructor(private ordersService: OrdersService) { }
+  
+  ngOnInit(): void {
+    this.getOrders();
+  }
+
+  getOrders() {
+    this.ordersService.getOrdersForUser().subscribe({
+      next: orders => this.orders = orders
+    });
+  }
 
 }
